5720263ab4091f2e1c6cd54b867d056bae3879aa
Yash Kadam <yashkadam278@gmail.com>
2023-10-02 10:52:33 +0530

    Nerw update

--- flies.sh
+++ flies.sh
@@ -1,6 +1,10 @@
-for f in ./images/newCarousel/*.jpg
+for f in ./images/passionFruitCarousel/*;
 do
   # always double quote "$f" filename
   # do something on $f
-  echo "<div class='carousel-item' data-interval='2000'><img src='$f' class='d-block w-100' alt='...'/></div>" >> greetings.txt
+  echo "<div class='carousel-item' data-interval='2000'>
+          <a data-toggle="lightbox" data-gallery="gallery-passion" href='$f'>
+            <img src='$f' class='d-block w-100' alt='...'/>
+          </a>
+        </div>" >> greetings.txt
 done
